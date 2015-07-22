ba872736b811628994b224ed4a5d1138ab212b0a
bankim <bankim@motifworks.com>
2015-07-22 10:45:33 +0530

    eventbrief template2

--- client/iOS/event/event.js
+++
@@ -1,6 +0,0 @@
-Template.eventIos.helpers({
-sessionsbyeventid: function(){
- var eid=Router.current().params.query.EventId;
-     return	Sessions.find({eventid:eid});//{eventid:eid}
-}
-});
---
+++ client/iOS/session/sessionlist_ios.js
@@ -0,0 +1,6 @@
+Template.eventIos.helpers({
+sessionsbyeventid: function(){
+ var eid=Router.current().params.query.EventId;
+     return	Sessions.find({eventid:eid});//{eventid:eid}
+}
+});
