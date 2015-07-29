041a55e3a11ab961e2509890a984df32c7649bf7
prashanthmotifworks <prashanth@motifworks.com>
2015-07-29 18:22:21 +0530

    removing spaces

--- client/home/_summary/summary.js
+++ client/home/_summary/summary.js
@@ -47,31 +47,24 @@ Template.summary.helpers({
 Template.summary.events = {
 	'click #btnspkemail': function(){
 		var event = Events.findOne({_id:Session.get('eventid')});
-		for (i = 0; i < event.speakers.length; i++) {
-      
+		for (i = 0; i < event.speakers.length; i++) {     
 
 			var html = "Hello "+ event.speakers[i].fname +" " + event.speakers[i].lname ;
-			 html+= " Please login with your email and Appcode";
-			 html+= "<br/> AppCode : "+ event.redeemnumber; 
+			html+= " Please login with your email and Appcode";
+			html+= "<br/> AppCode : "+ event.redeemnumber; 
+			Meteor.call('sendEmail', "Event:"+ event.title +" Invitation", html,event.speakers[i].email);
+		}
+	},
 
-	   	Meteor.call('sendEmail', "Event:"+ event.title +" Invitation", 
+	'click #btnatnemail': function(){
+		var event = Events.findOne({_id:Session.get('eventid')});
 
-	   		html,
+		for (i = 0; i < event.attendees.length; i++) {
+			Meteor.call('sendEmail', "Event:"+ event.title +" Invitation", 
+				"<h2>"+event.title+"</h2> Attendee Invitation",
+				event.attendees[i].email);
+		}
 
-	   		 event.speakers[i].email);
 	}
-},
-
-'click #btnatnemail': function(){
-	var event = Events.findOne({_id:Session.get('eventid')});
-
-	for (i = 0; i < event.attendees.length; i++) {
-	   Meteor.call('sendEmail', "Event:"+ event.title +" Invitation", 
-	   	"<h2>"+event.title+"</h2> Attendee Invitation",
-	   	event.attendees[i].email);
-	}
-
-}
-
 
 }
