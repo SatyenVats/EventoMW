91d5b6da37271ae3f3219d1ee4936ab5a508dd56
bankim <bankim@motifworks.com>
2015-08-11 17:46:40 +0530

    bottom bar icon has been replaced

--- client/iOS/sponsors/sponsors.js
+++ client/iOS/sponsors/sponsors.js
@@ -3,6 +3,9 @@ Template.sponsorsios.helpers({
        var eid=Router.current().params.query.EventId;
       return Events.find({_id:eid});
 
+       },
+       currenteventid: function(){
+         return Router.current().params.query.EventId;
        }
 });
 Template.sponsorsios.events({
